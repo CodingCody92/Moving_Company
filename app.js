b16b96a2d5166692aad4e4667f313ecbf7c7bd25
Cody <alexhill318@yahoo.com>
2021-11-29 16:51:52 -0600

    new commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const links = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    links.classList.toggle('active')
+})
+
