81f7f1ed7f8dd58fa65a62b979a6c05a72a9142f
Cody <alexhill318@yahoo.com>
2021-12-03 11:49:47 -0600

    new commit

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const links = document.getElementsByClassName('navbar-links')[0]
+const buttons = document.querySelector('.btn')
 
 toggleButton.addEventListener('click', () => {
     links.classList.toggle('active')
 })
 
+buttons.addEventListener('click', () => {
+    buttons.innerHTML = 'Simple Booking'
+
+    setTimeout(() => {
+        buttons.innerHTML = 'Easy Booking'
+    }, 3000);
+})
